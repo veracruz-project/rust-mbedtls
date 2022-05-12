129eedf4fbf6909d8e01fc164786c621aea7168e
Edmund Grimley Evans <edmund.grimley-evans@arm.com>
2022-05-12 14:56:53 +0100

    Add TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305_SHA256 to enum CipherSuite.

--- mbedtls/src/ssl/ciphersuites.rs
+++ mbedtls/src/ssl/ciphersuites.rs
@@ -214,5 +214,6 @@ define!(
         EcdheEcdsaWithAes128Ccm8 = TLS_ECDHE_ECDSA_WITH_AES_128_CCM_8,
         EcdheEcdsaWithAes256Ccm8 = TLS_ECDHE_ECDSA_WITH_AES_256_CCM_8,
         EcjpakeWithAes128Ccm8 = TLS_ECJPAKE_WITH_AES_128_CCM_8,
+        EcdheEcdsaWithChacha20Poly1305Sha256 = TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305_SHA256,
     }
 );
