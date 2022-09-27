6224ee1c0d59188fe208094de58b8c65d6215b92
Guilhem Bryant <Guilhem.Bryant@arm.com>
2022-09-27 11:58:26 +0100

    Revert "Implement timeout on `getrandom()`: Exit after timeout"
    This reverts commit b5d7f4d2a0ea8429e14f3a121bdea78590fe5a0d.

--- mbedtls-sys/build/mbedtls_hardware_poll.c
+++ mbedtls-sys/build/mbedtls_hardware_poll.c
@@ -10,10 +10,7 @@
  *
  */
 
-#include "mbedtls/platform.h"
 #include "psa/crypto.h"
-#include <signal.h>
-#include <unistd.h>
 
 #if 0
 // This is what one would normally do to get the prototype for getrandom,
@@ -26,32 +23,11 @@
 ssize_t getrandom(void *buf, size_t buflen, unsigned int flags);
 #endif
 
-// Handle SIGALARM signal.
-void handle_sigalarm(int signo)
-{
-    (void)signo;
-    mbedtls_printf("getrandom() could not return before the timeout. The entropy bug might be back to haunt us! Aborting\n");
-    mbedtls_exit(1);
-}
-
 int mbedtls_hardware_poll(void *data,
                           unsigned char *output, size_t len, size_t *olen)
 {
     (void)data;
-
-    // Configure a one-shot alarm that terminates the process with an error
-    // message if `getrandom()` doesn't return pronto.
-    // Cf. https://github.com/veracruz-project/veracruz/issues/507
-    // TODO: Remove this mechanism when the entropy issue goes away
-    struct sigaction act;
-    act.sa_handler = &handle_sigalarm;
-    act.sa_flags = SA_RESETHAND;
-    sigaction(SIGALRM, &act, NULL);
-    alarm(10);
-#endif
     ssize_t ret = getrandom(output, len, 0);
-    alarm(0);
-
     if (ret == -1)
         return PSA_ERROR_GENERIC_ERROR;
     *olen = ret;
